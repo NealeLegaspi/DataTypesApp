39397e127280ad27a2d54c5a03bea5e187f63722
legaspi.217303 <legaspi.217303@LAB-A-PC22>
2024-02-16 09:44:03 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace DataTypesApp
+{
+    class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the pieces of pieces of apple: ");
+            int applePieces = int.Parse(Console.ReadLine());
+
+            Console.Write($"Enter the total of prices of {applePieces} apple(s): ");
+            double priceTotal = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The total price of {applePieces} apples(s) is {priceTotal}");
+            int convertedPrice = (int)priceTotal;
+
+            Console.WriteLine($"The value of original price is {priceTotal}");
+            Console.WriteLine($"The value of converted price is {convertedPrice}");
+
+            Console.WriteLine("Press any key to exit.....");
+            Console.ReadKey();
+
+        }
+    }
+}
